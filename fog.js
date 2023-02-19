a920110a2353f0757f82235cebbf0e5e93cca338
Grant Garrett-Grossman <grantlycee@gmail.com>
2023-02-19 17:55:34 -0600

    Added working click callback for ingredient.

--- fog.js
+++ fog.js
@@ -1,3 +1,11 @@
 // function drag(ev) {
 //     ev.dataTransfer.setData("text", ev.target.id);
-// } 
+// }
+window.onload = function() {
+    let el = document.getElementsByClassName('ingredient_1')[0];
+
+    el.addEventListener('click', (event) => {
+        el.style.display = "none"
+    });
+
+};
