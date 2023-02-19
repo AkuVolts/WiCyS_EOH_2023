c3d8b8cfbd460085c95f38136e257f6116db9e8e
Jacob Mueller <jacobam3@illinois.edu>
2023-02-19 17:43:31 -0600

    EOH meeting work

--- ike.js
+++ ike.js
@@ -1,11 +1,26 @@
 function exitIke() {
-    alert("This will return to main screen.");
+    // TODO
+    //alert("This will return to main screen.");
+    if (confirm("Would you like to return to leave Ikenberry Dining Hall?")) {
+        // leave
+    } else {
+        // stay
+    }
 }
 
+laptop_interacted_flag = false;
+
 function laptop() {
-    alert("This will control the laptop.")
+    //alert("This will control the laptop.")
+    laptop_interacted_flag = confirm("Interact with laptop?");
+    //laptop_interacted_flag = true;
 }
 
 function swipeIn() {
-    alert("This will trigger the swipe in event.")
+    //alert("This will trigger the swipe in event.")
+    if (laptop_interacted_flag) {
+        alert("Awesome, you have some meals left on your account. Have a good lunch!");
+    } else {
+        alert("Sorry, you have no more meals left on your account. You cannot enter the dining hall unless you pay for more meals.")
+    }
 }
