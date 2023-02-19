a01c1404f7da08ae6f7df58541c14784282f8118
muelljac99 <muelljac99@gmail.com>
2023-02-18 19:48:47 -0600

    Clickable areas moved to correct locations and js functions created and linked.

---
+++ ike.js
@@ -0,0 +1,11 @@
+function exitIke() {
+    alert("This will return to main screen.");
+}
+
+function laptop() {
+    alert("This will control the laptop.")
+}
+
+function swipeIn() {
+    alert("This will trigger the swipe in event.")
+}
